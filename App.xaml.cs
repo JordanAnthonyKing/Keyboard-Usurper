11d6ce32f05fc075f135492690962dfac13e04ec
Jordan Cross <jordan.cross@ihsmarkit.com>
2021-04-27 21:23:09 +0100

    Functional Hook

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Keyboard_Usurper
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+        [STAThread]
+		private void Usurp(object sender, StartupEventArgs e)
+		{
+			MainWindow main = new MainWindow();
+			main.Show();
+		}		
+
+	}
+}
---
+++ KeyboardHook.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Windows.Sdk;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using Microsoft.Win32.SafeHandles;
+
+namespace Keyboard_Usurper
+{
+	class KeyboardHook
+	{
+		UnhookWindowsHookExSafeHandle _hookHandle = null;
+		HOOKPROC _hookFunction = null;
+
+		// public delegate void HookEventHandler(object sender, HookEventArgs e);
+		// public event HookEventHandler KeyDown;
+		// public event HookEventHandler KeyUp;
+
+		public KeyboardHook()
+		{
+			_hookFunction = new HOOKPROC(HookCallBack);
+			Install();
+		}
+
+		// private LRESULT HookCallBack(int code, IntPtr wParam, ref KBDLLHOOKSTRUCT lParam)
+		private LRESULT HookCallBack(int code, WPARAM wParam, LPARAM lParam)
+		{
+			if (code < 0) return PInvoke.CallNextHookEx(_hookHandle, code, wParam, lParam);
+
+			// KeyUp event
+			// if ((lParam.flags & 0x80) != 0 && this.KeyUp != null)
+			// this.KeyUp(this, new HookEventArgs(lParam.vkCode));
+
+			if (code == 0 && wParam.Value == Constants.WM_KEYDOWN) { System.Diagnostics.Debug.WriteLine("YOLO"); } 
+
+			// KeyDown event
+			// if ((lParam.flags & 0x80) == 0 && this.KeyDown != null)
+			// this.KeyDown(this, new HookEventArgs(lParam.vkCode));
+			
+			System.Diagnostics.Debug.WriteLine("Hello World!");
+
+			return PInvoke.CallNextHookEx(_hookHandle, code, wParam, lParam);
+
+		}
+
+		private void Install()
+		{
+			// make sure not already installed
+			if (_hookHandle != null)
+				return;
+
+			_hookHandle = PInvoke.SetWindowsHookEx(
+				SetWindowsHookEx_idHook.WH_KEYBOARD_LL,
+				_hookFunction,
+				new SafeProcessHandle(PInvoke.GetModuleHandle((string)null), true),
+				0);
+
+			System.Diagnostics.Debug.WriteLine("Something to break on");
+		}
+
+		private void Uninstall()
+		{
+			if (_hookHandle != null)
+			{
+				// uninstall system-wide hook
+				_hookHandle.Dispose();
+			}
+		}
+	}
+}
