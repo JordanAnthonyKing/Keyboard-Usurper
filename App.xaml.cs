f78bcdda3f8fcde314b1a3dec9047fcab407535c
Jordan King <jordan@jordanking.dev>
2021-05-02 21:20:55 +0100

    Convert json config to mapping

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
 
 namespace Keyboard_Usurper
 {
@@ -13,21 +16,15 @@ namespace Keyboard_Usurper
 	/// </summary>
 	public partial class App : Application
 	{
-
 		private KeyboardHook _hook;
 
         [STAThread]
 		private void Usurp(object sender, StartupEventArgs e)
 		{
-			_hook = new KeyboardHook(
-				new Mapping
-				{
-					Mappings = new List<KeyToKey>()
-					{
-						new KeyToKey { From = vkCode.VK_NUMPAD0, To = vkCode.VK_NUMPAD9 }
-					}
-				}
-			); ;
+			string jsonString = File.ReadAllText(Path.Combine(Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%"), "usurper.json")); 
+			Configuration config = JsonSerializer.Deserialize<Configuration>(jsonString);
+
+			_hook = new KeyboardHook(ConfigurationToMapping.Convert(config));
 
 			MainWindow main = new MainWindow();
 			main.Show();
--- Mapping.cs
+++ Mapping.cs
@@ -8,12 +8,51 @@ namespace Keyboard_Usurper
 {
 	public class Mapping
 	{
-		public List<KeyToKey> Mappings;
+		public List<KeyToKey> Mappings = new();
 	}
 
 	public class KeyToKey
 	{
-		public vkCode From;
-		public vkCode To;
+		public Key From;
+		public Key To;
+	}
+
+	public class Key
+	{
+		public vkCode[] Mods;
+		public vkCode Code;
+	}
+
+	public static class ConfigurationToMapping
+	{
+		public static Mapping Convert(Configuration config)
+		{
+			Mapping mapping = new();
+
+			// TODO: Handle ---
+			foreach(ConfigBinding binding in config.bindings)
+			{
+				string[] fromKeys = binding.from.Split('-');
+				string[] toKeys = binding.to.Split('-');
+
+				KeyToKey k2k = new()
+				{
+					From = new Key
+					{
+						Mods = fromKeys.Take(fromKeys.Length - 1).Select(x => StringToCode.ConvertTo(x)).ToArray(),
+						Code = StringToCode.ConvertTo(fromKeys.Last())
+					},
+					To = new Key
+					{
+						Mods = toKeys.Take(toKeys.Length - 1).Select(x => StringToCode.ConvertTo(x)).ToArray(),
+						Code = StringToCode.ConvertTo(toKeys.Last())
+					}
+				};
+
+				mapping.Mappings.Add(k2k);
+			}
+
+			return mapping;
+		}
 	}
 }
