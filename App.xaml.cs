2ecaa246744cfc6b7860ecac011ff2849dd7ffb1
Jordan King <jordan@jordanking.dev>
2021-10-26 21:27:03 +0100

    Initial system tray work

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,15 +8,19 @@ using System.Windows;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.IO;
+using System.Windows.Forms;
 
 namespace Keyboard_Usurper
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App : Application
-	{
+	public partial class App : System.Windows.Application
+    {
 		private KeyboardHook _hook;
+		// Magic never read code that works
+        private NotifyIcon _notifyIcon;
+		private event EventHandler _test;
 
         [STAThread]
 		private void Usurp(object sender, StartupEventArgs e)
@@ -26,13 +30,32 @@ namespace Keyboard_Usurper
 
 			_hook = new KeyboardHook(ConfigurationToMapping.Convert(config));
 
-			MainWindow main = new MainWindow();
-			main.Show();
+			// _test += TestEventHandler;
+
+            var contextMenuStrip = new ContextMenuStrip();
+			contextMenuStrip.Items.Add("Usurped", null, TestEventHandler);
+
+			_notifyIcon = new NotifyIcon
+            {
+                Text = "Keyboard Usurper",
+                Icon = Keyboard_Usurper.Properties.Resources.crown,
+                Visible = true,
+				ContextMenuStrip = contextMenuStrip
+            };
+
+			// MainWindow main = new MainWindow();
+			// main.Show();
 		}		
+
+		private void TestEventHandler(object sender, EventArgs e)
+        {
+			System.Diagnostics.Debug.WriteLine("working");
+        }
 
 		private void Unsurp(object sender, ExitEventArgs e)
 		{
 			_hook.Uninstall();
+			_notifyIcon.Dispose();
 		}
 	}
 }
