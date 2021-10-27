1f23001d490d1520371c0aed982cb4b09aaf4d24
Jordan King <jordan@jordanking.dev>
2021-10-27 19:52:48 +0100

    Sample with notify icon issue

---
+++ NotifyIconViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Keyboard_Usurper
+{
+    public class NotifyIconViewModel
+    {
+        /// <summary>
+        /// Shows a window, if none is already open.
+        /// </summary>
+        public ICommand ShowWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CanExecuteFunc = () => true,
+                    CommandAction = () =>
+                    {
+                        System.Diagnostics.Debug.WriteLine("Hellow World!");
+                    }
+                };
+            }
+        }
+
+        /// <summary>
+        /// Hides the main window. This command is only enabled if a window is open.
+        /// </summary>
+        public ICommand HideWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+
+                    CommandAction = () => System.Diagnostics.Debug.WriteLine("Hello World 2"),
+                    CanExecuteFunc = () => true
+                };
+            }
+        }
+
+
+        /// <summary>
+        /// Shuts down the application.
+        /// </summary>
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
+            }
+        }
+    }
+}
