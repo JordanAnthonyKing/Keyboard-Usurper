d87f1c2054c0bcc04e811029e6c92457c7acf74d
Jordan King <jordan@jordanking.dev>
2022-06-22 21:10:49 +0100

    Generate IDs for binding sets

--- Mapping.cs
+++ Mapping.cs
@@ -6,88 +6,106 @@ using System.Threading.Tasks;
 
 namespace Keyboard_Usurper
 {
-	// TODO: Make most of this stuff readonly or get not set
-	public class KeyToKey
-	{
-		public Key From;
-		public Key To;
-		// TODO: Figure out how to tie this to dynamic functions
-		// Or we could just give each a set ID? and if no To then add that set ID
-		// to disabled? Needs special case for the actual toggler...
-		// public Func<bool> Enabled;
-		// public Func<string, null> ToggleFunc;
+    public class KeyToKey
+    {
+        public Key From { get; }
+        public Key To { get; }
+        public int GroupId { get; }
 
-	}
+        public KeyToKey(Key from, Key to, int groupId)
+        {
+            From = from;
+            To = to;
+            GroupId = groupId;
+        }
+    }
 
-	public class Key
-	{
-		public IEnumerable<vkCode> Mods;
-		public vkCode ActivationKey;
-		public vkCode Code;
-		public bool WithMods;
-	}
+    public class Key
+    {
+        public IEnumerable<vkCode> Mods { get; }
+        public vkCode ActivationKey { get; }
+        public vkCode Code { get; }
+        public bool WithMods { get; }
 
-	public static class ConfigurationToMapping
-	{
-		// TODO: Make this an array?
-		public static List<KeyToKey> Convert(Configuration config)
-		{
-			List<KeyToKey> mappings = new();
-			vkCode[] mods = new vkCode[]{ 
-				vkCode.VK_LSHIFT,
-				vkCode.VK_RSHIFT,
-				vkCode.VK_SHIFT,
-				vkCode.VK_LWIN,
-				vkCode.VK_RWIN,
-				vkCode.VK_WIN,
-				vkCode.VK_LCONTROL,
-				vkCode.VK_RCONTROL,
-				vkCode.VK_CONTROL,
-				vkCode.VK_LMENU,
-				vkCode.VK_RMENU,
-				vkCode.VK_MENU
-			};
+        public Key(IEnumerable<vkCode> mods, vkCode activationKey, vkCode code, bool withMods)
+        {
+            Mods = mods;
+            ActivationKey = activationKey;
+            Code = code;
+            WithMods = withMods;
+        }
+    }
 
-			// TODO: Handle ---
-			foreach(ConfigBinding binding in config.bindings)
-			{
-				string[] fromKeys = binding.from.Split('-');
-				string[] toKeys = binding.to.Split('-');
+    public static class ConfigurationToMapping
+    {
+        // TODO: Make this an array?
+        public static List<KeyToKey> Convert(Configuration config)
+        {
+            List<KeyToKey> mappings = new();
+            vkCode[] modifiers = new vkCode[]{
+                vkCode.VK_LSHIFT,
+                vkCode.VK_RSHIFT,
+                vkCode.VK_SHIFT,
+                vkCode.VK_LWIN,
+                vkCode.VK_RWIN,
+                vkCode.VK_WIN,
+                vkCode.VK_LCONTROL,
+                vkCode.VK_RCONTROL,
+                vkCode.VK_CONTROL,
+                vkCode.VK_LMENU,
+                vkCode.VK_RMENU,
+                vkCode.VK_MENU
+            };
 
-				KeyToKey k2k = new()
-				{
-					From = new Key
-					{
-						Mods = fromKeys
-						  .Take(fromKeys.Length - 1)
-						  .Select(x => StringToCode.ConvertTo(x))
-						  .Where(x => mods.Contains(x)),
-						// TODO: A better way to do this?
-						ActivationKey = fromKeys
-						  .Take(fromKeys.Length - 1)
-						  .Select(x => StringToCode.ConvertTo(x))
-						  .Where(x => !mods.Contains(x))
-						  .FirstOrDefault(),
-						Code = StringToCode.ConvertTo(fromKeys.Last())
-					},
-					To = new Key
-					{
-						// TODO: Some code to detect single mod keys
-						Mods = toKeys
-							.Take(toKeys.Length - 1)
-							.Select(x => StringToCode.ConvertTo(x))
-							.Where(x => mods.Contains(x)),
-						Code = toKeys.Last()[0] == '+' ?
-  						StringToCode.ConvertTo(toKeys.Last().Substring(1)) :
-						StringToCode.ConvertTo(toKeys.Last()),
-						WithMods = toKeys.Last()[0] == '+'
-					}
-				};
+            Func<string[], Key> createFromKey = delegate (string[] keys)
+            {
 
-				mappings.Add(k2k);
-			}
+                IEnumerable<vkCode> mods = keys
+                      .Take(keys.Length - 1)
+                      .Select(x => StringToCode.ConvertTo(x))
+                      .Where(x => modifiers.Contains(x));
+                // TODO: A better way to do this?
+                vkCode activationKey = keys
+                      .Take(keys.Length - 1)
+                      .Select(x => StringToCode.ConvertTo(x))
+                      .FirstOrDefault(x => !modifiers.Contains(x));
+                vkCode code = StringToCode.ConvertTo(keys.Last());
 
-			return mappings;
-		}
-	}
+                return new Key(mods, activationKey, code, false);
+            };
+
+            int count = 0;
+            Func<int> getGroupId = delegate ()
+            {
+                count++;
+                return count;
+            };
+
+            // TODO: Handle ---
+            foreach (BindingSet set in config.bindings)
+            {
+                int groupId = getGroupId();
+
+                mappings.Add(new KeyToKey(createFromKey(set.toggleBinding.Split('-')), null, groupId));
+
+                foreach (ConfigBinding binding in set.bindings)
+                {
+                    string[] toKeys = binding.to.Split('-');
+
+                    IEnumerable<vkCode> mods = toKeys
+                        .Take(toKeys.Length - 1)
+                        .Select(x => StringToCode.ConvertTo(x))
+                        .Where(x => modifiers.Contains(x));
+                    vkCode code = toKeys.Last()[0] == '+' ?
+                        StringToCode.ConvertTo(toKeys.Last().Substring(1)) :
+                        StringToCode.ConvertTo(toKeys.Last());
+                    bool withMods = toKeys.Last()[0] == '+';
+
+                    mappings.Add(new KeyToKey(createFromKey(binding.from.Split('-')), new Key(mods, vkCode.VK_NULL, code, withMods), groupId));
+                }
+            }
+
+            return mappings;
+        }
+    }
 }
